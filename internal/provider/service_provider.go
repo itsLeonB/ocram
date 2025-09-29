ba5faac29b438e2ed5853c96f53d9351ca5736e5
itsLeonB <ellionblessan@gmail.com>
2025-09-29 17:32:01 +0700

    fix: return early if no task

--- internal/provider/service_provider.go
+++ internal/provider/service_provider.go
@@ -1,6 +1,7 @@
 package provider
 
 import (
+	"github.com/itsLeonB/ezutil/v2"
 	"github.com/itsLeonB/ocram/internal/service"
 	"github.com/rotisserie/eris"
 )
@@ -9,18 +10,22 @@ type Services struct {
 	ExpenseBill service.ExpenseBillService
 }
 
-func ProvideServices(clients *Clients, queues *Queues) (*Services, error) {
+func ProvideServices(clients *Clients, queues *Queues, logger ezutil.Logger) (*Services, error) {
 	if clients == nil {
 		return nil, eris.New("clients cannot be nil")
 	}
 	if queues == nil {
 		return nil, eris.New("queues cannot be nil")
 	}
+	if logger == nil {
+		return nil, eris.New("logger cannot be nil")
+	}
 
 	expenseBill, err := service.NewExpenseBillService(
 		queues.ExpenseBillTextExtracted,
 		queues.ExpenseBillUploaded,
 		clients.OCR,
+		logger,
 	)
 	if err != nil {
 		return nil, err
--- internal/service/expense_bill_service.go
+++ internal/service/expense_bill_service.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 
+	"github.com/itsLeonB/ezutil/v2"
 	"github.com/itsLeonB/meq"
 	"github.com/itsLeonB/ocram/internal/client"
 	"github.com/itsLeonB/ocram/internal/config"
@@ -18,12 +19,14 @@ type expenseBillServiceImpl struct {
 	textExtractedQueue meq.TaskQueue[message.ExpenseBillTextExtracted]
 	uploadedQueue      meq.TaskQueue[message.ExpenseBillUploaded]
 	ocr                client.OCRClient
+	logger             ezutil.Logger
 }
 
 func NewExpenseBillService(
 	textExtractedQueue meq.TaskQueue[message.ExpenseBillTextExtracted],
 	uploadedQueue meq.TaskQueue[message.ExpenseBillUploaded],
 	ocr client.OCRClient,
+	logger ezutil.Logger,
 ) (ExpenseBillService, error) {
 	if textExtractedQueue == nil || uploadedQueue == nil {
 		return nil, eris.New("queue cannot be nil")
@@ -35,6 +38,7 @@ func NewExpenseBillService(
 		textExtractedQueue,
 		uploadedQueue,
 		ocr,
+		logger,
 	}, nil
 }
 
@@ -42,6 +46,11 @@ func (ebs *expenseBillServiceImpl) ExtractBillText(ctx context.Context) error {
 	task, taskID, err := ebs.uploadedQueue.GetOldest(ctx)
 	if err != nil {
 		return err
+	}
+
+	if task.IsZero() || taskID == "" {
+		ebs.logger.Info("no expense bill newly uploaded")
+		return nil
 	}
 
 	text, err := ebs.ocr.ExtractFromURI(ctx, task.Message.URI)
